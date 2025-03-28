9835921273c65a7a84f495ced141a50c10712bcc
Dreamlord <dr.swordshield@gmail.com>
2025-03-28 13:32:19 +0500

    браузер больше не закрывается

--- webdriver.py
+++ webdriver.py
@@ -7,6 +7,7 @@ from webdriver_manager.core.os_manager import ChromeType
 def get_driver():
     chrome_service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     # chrome_options.add_argument("--headless")  # Включите для скрытого режима
