1aeb602135b03e3b4ac8676a88363e61cd2f55fc
Dreamlord <dr.swordshield@gmail.com>
2025-03-30 22:08:46 +0500

    chrome популярнее chromium

--- webdriver.py
+++ webdriver.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 def get_driver():
-    chrome_service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    chrome_service = ChromiumService(ChromeDriverManager().install())
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--log-level=3")
