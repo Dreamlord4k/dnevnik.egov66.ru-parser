c01f1d4c989f8a9297663340aef225a55966c3fc
Dreamlord <dr.swordshield@gmail.com>
2025-03-30 13:14:46 +0500

    headless

--- webdriver.py
+++ webdriver.py
@@ -10,6 +10,6 @@ def get_driver():
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    # chrome_options.add_argument("--headless")  # Включите для скрытого режима
+    chrome_options.add_argument("--headless")  # Включите для скрытого режима
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     return driver
